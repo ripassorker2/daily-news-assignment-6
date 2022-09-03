73dde1d96cc4b009cbce376f6e0daf1c0ecc2cfa
Ripassorker <ripassorker02@glail.com>
2022-09-03 20:00:18 +0600

    added spinner

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let loadedAllCatagoryNews = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
         .then(data => displayAllCatagoryNews(data.data.news_category))
+        .catch(error => console.log(error))
 }
 
 let displayAllCatagoryNews = (allCatagory) => {
@@ -10,21 +11,26 @@ let displayAllCatagoryNews = (allCatagory) => {
     allCatagory.forEach(catagory => {
         let singleCountryName = document.createElement('div')
         singleCountryName.innerHTML = `
-            <a onclick="loadedSingleCatagoryDetails(${catagory.category_id})"  class="fs-5 text-decoration-none text-dark fw-bold" href="#">${catagory.category_name}</a>
+            <a onclick="loadedSingleCatagoryDetails(${catagory.category_id})"  class="fs-5 px-3 text-decoration-none text-primary fw-bold" href="#">${catagory.category_name}</a>
             `
         allCatagoryName.appendChild(singleCountryName)
     });
-
-
 }
+
+
 let loadedSingleCatagoryDetails = (id) => {
-    // console.log(id)
+    toggleSpinner(true);
     fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
         .then(res => res.json())
         .then(data => displaySingleCatagoryNews(data.data))
+        .catch(error => console.log(error))
+
 }
 
+
+
 let displaySingleCatagoryNews = (singleCatagoryNews) => {
+
     let perNewsContainer = document.getElementById('per-news');
     perNewsContainer.innerHTML = '';
     singleCatagoryNews.forEach(info => {
@@ -38,7 +44,7 @@ let displaySingleCatagoryNews = (singleCatagoryNews) => {
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title fw-bold">${info.title}</h5>
+                    <h5 class="card-title text-info fw-bold">${info.title}</h5>
                     <p class="card-text">${info.details.slice(0, 200)}...</p>
             </div>
             <div class="container d-flex justify-content-between my-5 flex-flex-wrap card-footer">
@@ -65,18 +71,25 @@ let displaySingleCatagoryNews = (singleCatagoryNews) => {
    
         `
         perNewsContainer.appendChild(div)
+
     });
+    toggleSpinner(false)
 
 }
+
+
 let loadedNewsDetails = (id) => {
     fetch(`https://openapi.programming-hero.com/api/news/${id}`)
         .then(res => res.json())
         .then(data => displayNewsDetails(data.data))
-
-
+        .catch(error => console.log(error))
 }
 
+
+
+
 let displayNewsDetails = (newsDetails) => {
+
     console.log(newsDetails)
     newsDetails.forEach(news_info => {
         console.log(news_info)
@@ -84,15 +97,29 @@ let displayNewsDetails = (newsDetails) => {
         newsTitle.innerText = `News title : ${news_info.title} `
         let modalBody = document.getElementById('modal-body')
         modalBody.innerHTML = `
-                <img src="${news_info.image_url ? news_info.image_url : 'no bannar found'}"                  class="img-img-fluid w-100" alt="">
+                <img src="${news_info.image_url ? news_info.image_url : 'no bannar found'}"                  class="img-img-fluid w-100 pb-3" alt="">
                 <h6>Author details :</h6>
                 <img src="${news_info.author.img ? news_info.author.img : 'No author image found'}"          class="img-img-fluid rounded-circle"  style="height: 60px; width: 60px; alt= "">
-                <p>Name : ${news_info.author.name ? news_info.author.name : 'No author found'}</p>
+                <p>Name : ${news_info.author.name ? news_info.author.name : 'No author name found'}</p>
                 <p>Publish date : ${news_info.author.published_date ? news_info.author.published_date : 'No date  found'}</p>
                 <p>Total views : ${news_info.total_view ? news_info.total_view : 'No views'}M</p>
-                <p><strong>Description :</strong> ${news_info.details ? news_info.details : 'No views'}M</p>
+                <p><strong>Description :</strong> ${news_info.details ? news_info.details : 'No details available'}M</p>
         `
+
     })
+
+
+}
+
+
+let toggleSpinner = (isLoading) => {
+    let lodar = document.getElementById('lodar');
+    if (isLoading) {
+        lodar.classList.remove('d-none')
+    }
+    else {
+        lodar.classList.add('d-none');
+    }
 }
 
 loadedAllCatagoryNews();
