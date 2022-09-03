335865697be91db055c00382287f8aeb9aa04dd1
Ripassorker <ripassorker02@glail.com>
2022-09-03 16:23:40 +0600

    added modal

--- script.js
+++ script.js
@@ -27,9 +27,7 @@ let loadedSingleCatagoryDetails = (id) => {
 let displaySingleCatagoryNews = (singleCatagoryNews) => {
     let perNewsContainer = document.getElementById('per-news');
     perNewsContainer.innerHTML = '';
-    console.log(singleCatagoryNews)
     singleCatagoryNews.forEach(info => {
-        console.log(info)
         let div = document.createElement('div');
         div.classList.add('col-md-12',);
         div.innerHTML = `
@@ -42,8 +40,8 @@ let displaySingleCatagoryNews = (singleCatagoryNews) => {
                 <div class="card-body">
                     <h5 class="card-title fw-bold">${info.title}</h5>
                     <p class="card-text">${info.details.slice(0, 200)}...</p>
-                </div>
-                <div class="container d-flex justify-content-between my-5 flex-flex-wrap">
+            </div>
+            <div class="container d-flex justify-content-between my-5 flex-flex-wrap card-footer">
                 <div class="d-flex align-items-center ">
                     <img class="rounded-circle " style="height: 60px; width: 60px; " src="${info.author.img}" alt="">
                     <div class="ps-3">
@@ -58,8 +56,8 @@ let displaySingleCatagoryNews = (singleCatagoryNews) => {
                     <p> <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i><i class="fa-regular fa-star"></i> ${info.rating.number}</p>
                 </div>
                 <div class="pt-3 text-info">
-                    <h6>More details <i class="fa-solid fa-arrow-right"></i></h6>
-                </div>
+                    <button onclick="loadedNewsDetails('${info._id}')" class="btn btn-info"  data-bs-toggle="modal" data-bs-target="#loadedNewsDetails" >More details <i class="fa-solid fa-arrow-right"></i></button>
+            </div>
             </div>
             </div>
         </div>
@@ -69,6 +67,32 @@ let displaySingleCatagoryNews = (singleCatagoryNews) => {
         perNewsContainer.appendChild(div)
     });
 
+}
+let loadedNewsDetails = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data))
+
+
+}
+
+let displayNewsDetails = (newsDetails) => {
+    console.log(newsDetails)
+    newsDetails.forEach(news_info => {
+        console.log(news_info)
+        let newsTitle = document.getElementById('loadedNewsDetailsLabel');
+        newsTitle.innerText = `News title : ${news_info.title} `
+        let modalBody = document.getElementById('modal-body')
+        modalBody.innerHTML = `
+                <img src="${news_info.image_url ? news_info.image_url : 'no bannar found'}"                  class="img-img-fluid w-100" alt="">
+                <h6>Author details :</h6>
+                <img src="${news_info.author.img ? news_info.author.img : 'No author image found'}"          class="img-img-fluid rounded-circle"  style="height: 60px; width: 60px; alt= "">
+                <p>Name : ${news_info.author.name ? news_info.author.name : 'No author found'}</p>
+                <p>Publish date : ${news_info.author.published_date ? news_info.author.published_date : 'No date  found'}</p>
+                <p>Total views : ${news_info.total_view ? news_info.total_view : 'No views'}M</p>
+                <p><strong>Description :</strong> ${news_info.details ? news_info.details : 'No views'}M</p>
+        `
+    })
 }
 
 loadedAllCatagoryNews();
